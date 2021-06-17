91dc6171ebd7005dac94c9d182b3ff1a84168a8f
SokolMks <sokolenkomks@gmail.com>
2021-06-17 15:42:09 +0300

    Redone timer as a Class

--- js/index.js
+++ js/index.js
@@ -1,40 +1,69 @@
-const refs = {
-  days: document.querySelector('[data-value="days"]'),
-  hours: document.querySelector('[data-value="hours"]'),
-  mins: document.querySelector('[data-value="mins"]'),
-  secs: document.querySelector('[data-value="secs"]'),
-};
+// const refs = {
+//   days: document.querySelector('[data-value="days"]'),
+//   hours: document.querySelector('[data-value="hours"]'),
+//   mins: document.querySelector('[data-value="mins"]'),
+//   secs: document.querySelector('[data-value="secs"]'),
+// };
+// const timer = {
+//   start() {
+//     const endTime = new Date('Jul 17, 2021');   
+//     setInterval(() => {
+//       const startTime = Date.now();
+//       const finishedTime = endTime - startTime;
+//       const remainingTime = getTimeComponents(finishedTime);
+//       updateClock(remainingTime);
+//     }, 1000);
+//   },
+// };
+// timer.start();
+//   function updateClock({days, hours, mins, secs}) {
+//     refs.days.textContent = `${days}`;
+//     refs.hours.textContent = `${hours}`;
+//     refs.mins.textContent = `${mins}`;
+//     refs.secs.textContent = `${secs}`;
+//   }
 
-const timer = {
-  start() {
-    const endTime = new Date('Jul 17, 2021');
+  class EndTimer {
+    constructor({ selector, targetDate }) {
+      this.selector = selector;
+      this.targetDate = targetDate;
+    }
     
-    setInterval(() => {
-      const startTime = Date.now();
-      const finishedTime = endTime - startTime;
-      const remainingTime = getTimeComponents(finishedTime);
-      updateClock(remainingTime);
-
-    }, 1000);
-  },
-};
-
-timer.start();
-
-
-  function updateClock({days, hours, mins, secs}) {
-    refs.days.textContent = `${days}`;
-    refs.hours.textContent = `${hours}`;
-    refs.mins.textContent = `${mins}`;
-    refs.secs.textContent = `${secs}`;
+    start() {
+      const endTime = this.targetDate;
+      
+      setInterval(() => {
+        const startTime = Date.now();
+        const finishedTime = endTime - startTime;
+        const {days, hours, mins, secs} = getTimeComponents(finishedTime);
+        
+        const timer1 = document.querySelector('#timer-1');
+        
+        const day = timer1.querySelector('[data-value="days"]');
+        day.textContent = days;
+        
+        const hour = timer1.querySelector('[data-value="hours"]');
+        hour.textContent = hours;
+        
+        const min = timer1.querySelector('[data-value="mins"]');
+        min.textContent = mins;
+        
+        const sec = timer1.querySelector('[data-value="secs"]');
+        sec.textContent = secs;
+        
+      }, 1000);
+    }   
   }
-
-
+  
+  const timer = new Timer ({
+    selector: '#timer-1',
+    targetDate: new Date('Jul 17, 2021'),
+  })
+  timer.start();
 
   function pad(value) {
     return String(value).padStart(2, '0');
   }
-  
   
   function getTimeComponents(time) {
     const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
@@ -44,4 +73,4 @@ timer.start();
 
     return {days, hours, mins, secs};
     
-}
+  }
